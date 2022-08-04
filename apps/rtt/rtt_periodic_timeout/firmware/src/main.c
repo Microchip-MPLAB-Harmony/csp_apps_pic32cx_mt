4d99cf3777e211fadfe4d64fd36c3e5ccead575f
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-08-04 17:22:46 +0530

    [RTT] Add rtt_periodic_timeout application for PIC32CX_MTDB
    MH3-70205
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

---
+++ apps/rtt/rtt_periodic_timeout/firmware/src/main.c
@@ -0,0 +1,70 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+void Timeout_Handler (RTT_INTERRUPT_TYPE type, uintptr_t context)
+{
+    if(type == RTT_PERIODIC)
+    {
+        GPIO_PD18_Toggle();
+    }
+}
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+int main ( void )
+{ 
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+    RTT_CallbackRegister(Timeout_Handler, (uintptr_t) NULL);
+    RTT_Enable();
+    while ( true )
+    {
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
---
+++ apps/rtt/rtt_periodic_timeout/readme.md
@@ -0,0 +1,55 @@
+---
+parent: Harmony 3 peripheral library application examples for PIC32CXMTSH family
+title: RTT periodic interrupt 
+has_children: false
+has_toc: false
+---
+
+[![MCHP](https://www.microchip.com/ResourcePackages/Microchip/assets/dist/images/logo.png)](https://www.microchip.com)
+
+# RTT periodic interrupt
+
+This example shows how to use the RTT to generate periodic interrupts.
+
+## Description
+
+This example application shows how to configure the RTT Peripheral to generate periodic ticks. It toggles an LED every time the callback is triggered.
+
+## Downloading and building the application
+
+To clone or download this application from Github, go to the [main page of this repository](https://github.com/Microchip-MPLAB-Harmony/csp_apps_pic32cx_mt) and then click **Clone** button to clone this repository or download as zip file.
+This content can also be downloaded using content manager by following these [instructions](https://github.com/Microchip-MPLAB-Harmony/contentmanager/wiki).
+
+Path of the application within the repository is **apps/rtt/rtt_periodic_timeout/firmware** .
+
+To build the application, refer to the following table and open the project using its IDE.
+
+| Project Name      | Description                                    |
+| ----------------- | ---------------------------------------------- |
+| pic32cx_mtsh_db.X | MPLABX project for [PIC32CXMTSH-Development Board]() |
+|||
+
+## Setting up the hardware
+
+The following table shows the target hardware for the application projects.
+
+| Project Name| Board|
+|:---------|:---------:|
+| pic32cx_mtsh_db.X | [PIC32CXMTSH-Development Board]()
+|||
+
+### Setting up [PIC32CXMTSH-Development Board]()
+
+- Connect the Debug USB port on the board to the computer using a micro USB cable
+
+## Running the Application
+
+1. Build and Program the application project using its respective IDE
+2. The LED blinks continuously
+
+The following table provides the LED name:
+
+| Board      | LED |
+| ----------------- | ---------- |
+| [PIC32CXMTSH-Development Board]() | D20 |
+|||
